ab62ea98b73dfced5774cf02eacc003df6379d00
rnpvip <rnpvip@rnp.rnpvip>
2024-12-30 23:53:35 +0530

    first consept of OOPS classes

---
+++ OOPS.class.java
@@ -0,0 +1,11 @@
+class first{
+public  void method(){
+System.out.println("this is first class ");
+}
+}
+public class OOPS{
+public static void main(String args[]){
+first obj1=new first();
+obj1.method();
+}
+}
